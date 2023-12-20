8c3e947c66d638e8420f43e5763653afa255f5cf
nmjoselyne09 <nmjoselyne09@gmail.com>
2023-12-20 16:55:49 +0200

    some changes made on the registration form of charitable organisation

--- charitable.php
+++ charitable.php
@@ -1,9 +1,9 @@
 <?php
-//Database information
-$servername = "localhost"; 
-$username = "mulimuoki001"; 
-$password = "15121@Muli"; 
-$dbname = "mulimuoki001"; 
+// Database information
+$servername = "localhost";
+$username = "mulimuoki001";
+$password = "15121@Muli";
+$dbname = "mulimuoki001";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,12 +13,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
-
-
-
-$date = date('Y-m-d H:i:s');
-
 // Retrieve data from the HTML form
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -26,9 +20,7 @@ $phone = $_POST['phone'];
 $location = $_POST['location'];
 $capacity = $_POST['capacity'];
 $diet = $_POST['message'];
-$Schedule = $_POST['availability'];
-
-$date = date('Y-m-d H:i:s');
+$schedule = $_POST['availability'];
 
 // Remove any non-digit characters from the phone number
 $phone = preg_replace('/\D/', '', $phone);
@@ -37,27 +29,26 @@ $phone = preg_replace('/\D/', '', $phone);
 if (preg_match('/^\+?\d{1,3}[9\d]{2,13}$/', $phone)) {
     // Phone number structure is valid, proceed with storing the information in the database
 
-    // Hash the password
-   
-    // Insert data into the database
-    $sql = "INSERT INTO charitableorg (name, email,  phone, location, capacity, diet, Schedule,  date) VALUES ('$name', '$email',  '$phone', '$location', '$capacity', '$diet', '$Schedule', '$date')";
-    $result = $conn->query($sql);
-    if ($result === false) {
-        throw new Exception("Error executing query: " . $conn->error);
+    // Use prepared statement to prevent SQL injection
+    $sql = "INSERT INTO charitableorg (name, email, phone, location, capacity, diet, schedule, date) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('sssissi', $name, $email, $phone, $location, $capacity, $diet, $schedule);
+    $stmt->execute();
+    
+    // Check for errors
+    if ($stmt->error) {
+        echo "Error executing query: " . $stmt->error;
+    } else {
+        // Redirect to the thank you page
+        header("Location: thankyou.html");
+        exit();
     }
-
-    // Close connection
-    $conn->close();
-    header("Location: thankyou.html");
-    exit();
 } else {
     // Phone number structure is invalid, display an error message to the user
     echo "Invalid phone number format. Please enter a valid phone number.";
 }
 
-
 // Close connection
+$stmt->close();
 $conn->close();
-header("Location: thankyou.html");
-exit();
 ?>
